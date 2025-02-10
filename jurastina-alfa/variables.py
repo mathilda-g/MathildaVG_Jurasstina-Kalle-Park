62f780ef2031d9d4318d163ed0297d9bd5f65951
kskolos <kskolos@cox.net>
2025-02-10 10:57:23 +0100

    Added Directory, timeout and Browser variables

--- jurastina-alfa/variables.py
+++ jurastina-alfa/variables.py
@@ -1,1 +1,12 @@
 import os
+
+# Directory
+current_directory = os.getcwd().replace('\\', '/')
+
+# Timeouts
+standard_timeout = "10s"
+
+# Browser
+browser_demo = "chrome"
+url_demo = f"file:///{current_directory}/jurap.html"
+title_demo = "Jurap Demo"
