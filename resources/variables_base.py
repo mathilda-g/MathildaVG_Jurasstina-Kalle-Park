78f77b1aa93d4af69c565289147fe0b0bd8a45a4
Victor Ek <victor.ek@gmail.com>
2025-02-11 10:24:44 +0100

    added variables for register page

--- resources/variables_base.py
+++ resources/variables_base.py
@@ -13,3 +13,14 @@ register_link = "data:section:register-section"
 login_link = "data:section:login-section"
 buy_tickets_link = "data:section:tickets-section"
 book_safari_link = "data:section:safari-section"
+
+# register page
+register_section = "id:register-section" # use with keyword Element Should Be Visible
+register_username_input = "id:reg-username"
+register_password_input = "id:reg-password"
+register_form = "id:register-form" # use with keyword Submit Form
+register_message = "id:register-message"
+register_error_short_password = "Password must be at least 8 characters long."
+register_error_no_username_password = "Please enter both a username and password."
+register_error_username_already_exists = "Username already exists. Please choose another."
+register_success_message = "Registration successful! Redirecting to login..."
